ff550a9b5ff351de847f44a4053dfc060100d1b5
Son Nguyen <sonnguyen16@augustana.edu>
2020-04-19 21:26:36 -0500

    Basic design for login page

--- marketplace-react/src/views/Login/Login.js
+++ marketplace-react/src/views/Login/Login.js
@@ -1,8 +1,44 @@
 import { signInWithGoogle, auth } from "../../firebase/firebase";
 import React, { useState, useEffect, useReducer } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      height: '100vh',
+    },
+    image: {
+      backgroundImage: 'url(https://media-exp1.licdn.com/dms/image/C561BAQEECA8nKUOrgg/company-background_10000/0?e=2159024400&v=beta&t=Hbknoxy5g1jk-Ql6qb0DV-qrCOZ__2BLxxY_Y-x6dlU)',
+      backgroundRepeat: 'no-repeat',
+      backgroundColor:
+        theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
+    paper: {
+      margin: theme.spacing(8, 4),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 0),
+    },
+  }));
 
 export default function Login(props) {
     const [idToken, setIdToken] = useState(null)
+    const classes = useStyles();
 
     function signIn() {
         signInWithGoogle()
@@ -34,17 +70,41 @@ export default function Login(props) {
 
             console.log("idToken changed to:", idToken)
         })
-    }, [])
+    }, [idToken])
 
     return (
-        <div className="App">
-            <p>{auth.currentUser ? auth.currentUser.displayName + " is signed in" : "Please sign in"}</p>
+        <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+          </div>
+          <div className="App">
+            <p align="center"><h2>{auth.currentUser ? auth.currentUser.displayName + " is signed in" : "Please sign in"}</h2></p>
 
-            <button onClick={signIn}>Sign in with Google</button>
+            <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                onClick={auth.currentUser ? signOut : signIn}
+              >
+                {auth.currentUser ? "Sign Out" : "Sign In with Augustana Google Account"}
+            </Button>
+
+            {/* <button onClick={signIn}>Sign in with Google</button>
             <button onClick={signOut}>Sign me out</button>
 
             <p>The ID token is:</p>
-            <code>{auth.currentUser ? idToken : "Please sign in"}</code>
+            <code>{auth.currentUser ? idToken : "Please sign in"}</code> */}
         </div>
+        </Grid>
+      </Grid>
     );
 }
