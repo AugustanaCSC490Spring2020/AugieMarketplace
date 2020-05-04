c3ebbb37ab9d638f8dbc63fd7312ea0a817a8b2f
Samih Sghier <ssghier@scholastic.com>
2020-05-04 16:04:59 -0500

    took out test folder

--- augiemarketplaceapi /src/main/java/com/augiemarketplace/augiemarketplaceapi/AugiemarketplaceapiApplication.java
+++ augiemarketplaceapi /src/main/java/com/augiemarketplace/augiemarketplaceapi/AugiemarketplaceapiApplication.java
@@ -16,7 +16,7 @@ public class AugiemarketplaceapiApplication {
 	public static void main(String[] args) {
 		try {
 			FileInputStream serviceAccount =
-					new FileInputStream("firebase.json");
+					new FileInputStream("/Users/samihsghier/Desktop/SI/AugieMarketplace/augiemarketplaceapi /src/main/resources/firebase.json");
 
 			FirebaseOptions options = new FirebaseOptions.Builder()
 					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
