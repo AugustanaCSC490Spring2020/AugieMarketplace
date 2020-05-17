f4cb137b1c8cb121ef8f43ab7eeacb4049dc7197
Son Nguyen <sonnguyen16@augustana.edu>
2020-05-17 19:54:53 -0400

    update posting item

--- marketplace-react/src/views/CreatePostTest/FormDetail.js
+++ marketplace-react/src/views/CreatePostTest/FormDetail.js
@@ -18,16 +18,18 @@ export default function FormDetail(props) {
           id="itemPrice"
           name="itemPrice"
           label="Price"
+          type="number"
           fullWidth
           InputProps={{
           startAdornment: <InputAdornment position="start">$</InputAdornment>,
           }}
           onChange={props.handleChange}
           defaultValue={props.itemPrice}
+          helperText="Please enter a number"
         />
       </Grid>
       <Grid item xs={12} sm={6}>
-          <TextField id="itemType" label="Type" value="Other" name="itemType"
+          <TextField required id="itemType" label="Type" value={props.itemType} name="itemType"
                     onChange={props.handleChange}
                     defaultValue={props.itemType} select>
               <MenuItem value="Textbook">Textbook</MenuItem>
@@ -41,16 +43,20 @@ export default function FormDetail(props) {
           id="itemName"
           name="itemName"
           label="Name of Item"
+          inputProps={{ maxLength: 50 }}
           fullWidth
           onChange={props.handleChange}
           defaultValue={props.itemName}
+          helperText="Name of the item should be 50 characters or less"
         />
       </Grid>
       <Grid item xs={12}>
         <TextField
+          required
           id="itemDes"
           name="itemDes"
           label="Description"
+          multiline
           fullWidth
           onChange={props.handleChange}
           defaultValue={props.itemDes}
